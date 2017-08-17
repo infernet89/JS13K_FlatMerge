915f38c66f74bae680384173d357fff7f818afcc
cguarisc <claudio.guarisco@studenti.unipd.it>
2017-08-17 15:50:03 +0200

    It is something!

--- g.js
+++ g.js
@@ -10,6 +10,7 @@ var canvasH;
 var ctx;
 var activeTask;
 var level=1;
+var drawableObjects=[];
 
 //mobile controls
 var mousex=-100;
@@ -21,6 +22,22 @@ canvas = document.getElementById("g");
 ctx = canvas.getContext("2d");
 canvasW=canvas.width  = window.innerWidth;
 canvasH=canvas.height = window.innerHeight;
+
+for(i=0;i<200;i++)
+{
+    tmp=new Object();
+    tmp.size=16;
+    tmp.x=rand(tmp.size,canvasW-tmp.size);
+    tmp.y=rand(tmp.size*2,canvasH-tmp.size);
+    tmp.rotation=rand(0,360);
+    tmp.dx=rand(-8,8);
+    tmp.dy=rand(-8,8);
+    tmp.dr=rand(0,5);
+    tmp.type=rand(3,8);//actually, number of edges
+    
+    drawableObjects.push(tmp); 
+}
+
 
 /*if (window.navigator.pointerEnabled) {
     canvas.addEventListener("pointermove", mossoMouse, false);
@@ -41,17 +58,100 @@ activeTask=setInterval(run, 33);
 function run()
 {
 	ctx.clearRect(0, 0, canvasW, canvasH);
-    ctx.fillStyle="#000000";
+    ctx.fillStyle="#000";
     ctx.fillRect(0,0,canvasW,canvasH);
 
-    ctx.font = "8px Monospace";
     ctx.fillStyle="#FFF";
-    ctx.fillText("Made by: Infernet89",5,600);
+    ctx.fillRect(0,0,canvasW,1);
+    ctx.fillRect(0,canvasH-1,canvasW,1);
+    ctx.fillRect(0,0,1,canvasH);
+    ctx.fillRect(canvasW-1,0,1,canvasH);
 
     if(level==1)
     {
-        
+        drawableObjects.forEach(function(e)
+        {
+            drawOject(e);
+            e.x+=e.dx;
+            e.y+=e.dy;
+            e.rotation+=e.dr;
+            if(e.x+e.dx+e.size>canvasW || e.x+e.dx<e.size)
+                e.dx*=-1;
+            if(e.y+e.dy>canvasH || e.y+e.dy<e.size*2)
+                e.dy*=-1;
+        });
     }
+}
+function drawOject(o)
+{
+    var s=o.size;
+    ctx.strokeStyle="#FFF";
+    ctx.save();
+    ctx.translate(o.x-s,o.y-s);
+    ctx.rotate(o.rotation*Math.PI/180);
+    ctx.beginPath();
+    if(o.type==3)
+    {//triangle
+        ctx.moveTo(0,-s);
+        ctx.lineTo(-s,s);
+        ctx.lineTo(s,s);
+        ctx.lineTo(0,-s);
+    }
+    else if(o.type==4)
+    {//square
+        ctx.moveTo(-s,-s);
+        ctx.lineTo(s,-s);
+        ctx.lineTo(s,s);
+        ctx.lineTo(-s,s);
+        ctx.lineTo(-s,-s);
+    }
+    else if(o.type==5)
+    {//pentagon
+        ctx.moveTo(0,-s);
+        ctx.lineTo(s,-s/5);
+
+        ctx.lineTo(s/2,s);
+        ctx.lineTo(-s/2,s);
+
+        ctx.lineTo(-s,-s/5);
+        ctx.lineTo(0,-s);
+    }
+    else if(o.type==6)
+    {//hexagon
+        ctx.moveTo(-s,0);
+        ctx.lineTo(-s/2,-s);
+        ctx.lineTo(s/2,-s);
+        ctx.lineTo(s,0);
+        ctx.lineTo(s/2,s);
+        ctx.lineTo(-s/2,s);
+
+        ctx.lineTo(-s,0);
+    }
+    else if(o.type==7)
+    {//heptagon
+        ctx.moveTo(0,-s);
+        ctx.lineTo(s-s/5,-s/2);
+        ctx.lineTo(s,s/2);
+        ctx.lineTo(s/2,s);
+        ctx.lineTo(-s/2,s);
+        ctx.lineTo(-s,s/2);
+        ctx.lineTo(-s+s/5,-s/2);
+        ctx.lineTo(0,-s);
+    }
+    else if(o.type==8)
+    {//octagon
+        ctx.moveTo(-s/2,-s);
+        ctx.lineTo(s/2,-s);
+        ctx.lineTo(s,-s/2);
+        ctx.lineTo(s,s/2);
+        ctx.lineTo(s/2,s);
+        ctx.lineTo(-s/2,s);
+        ctx.lineTo(-s,s/2);
+        ctx.lineTo(-s,-s/2);
+        ctx.lineTo(-s/2,-s);
+    }
+    ctx.stroke();
+    ctx.restore();
 }
 /*#############
     Funzioni Utili
