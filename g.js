d59b2af62de861d3a5f46d8ff36997a36ab19554
Infernet89 <infernet2002@libero.it>
2017-09-03 18:35:02 +0200

    Need menu. And some level design. But we're almost there.

--- g.js
+++ g.js
@@ -9,7 +9,7 @@ var canvasW;
 var canvasH;
 var ctx;
 var activeTask;
-var level=1;
+var level=0;
 var drawableObjects=[];
 var selectedObject=null;
 var hoveredObject=null;
@@ -18,7 +18,7 @@ var cooldown;
 var mergeObjectA=null;
 var mergeObjectB=null;
 var mergeObjectC=null;
-
+var obstacleObjects=[]
 
 //mobile controls
 var mousex=-100;
@@ -47,14 +47,14 @@ canvas.addEventListener("mousemove",mossoMouse);
 canvas.addEventListener("mousedown",cliccatoMouse);
 canvas.addEventListener("mouseup",rilasciatoMouse);
 
+level=1;//TODO il menu, sullo ZERO.
 generateLevel();
 activeTask=setInterval(run, 33);
-
-
 
 function generateLevel()
 {
     drawableObjects=[];
+    obstacleObjects=[];
     //next level button
     tmp=new Object();
     tmp.size=20;
@@ -71,13 +71,17 @@ function generateLevel()
     exitObject=tmp;
     if(level==1)
     {
+        addObstacle(200,200,200,45,["LOST: unable to find the way.",
+                            "                  -Merriam-Webster"]);
+        addObstacle(150,300,430,30,["Sometimes, you don't even know what is the way you need to find"]);
         for(i=0;i<3;i++)
             addRandomObject(3);
-        for(i=0;i<7;i++)
+        for(i=0;i<77;i++)
             addRandomObject(2);
     }
     else if(level==2)
     {
+        addObstacle(150,200,340,45,["If you are feeling some confusion, it's perfectly fine.","It's really hard to find our way in life"]);
         for(i=0;i<7;i++)
             addRandomObject(3);
         for(i=0;i<3;i++)
@@ -85,6 +89,9 @@ function generateLevel()
     }
     else if(level==3)
     {
+        addObstacle(150,200,650,45,["Have you ever tried to hit something that is moving, while you are also moving in a different direction?",
+                                    "That's what finding our way in life feels like."]);
+        addObstacle(420,500,240,30,["Because we're all work-in-progress."]);
         for(i=0;i<15;i++)
             addRandomObject(3);
         for(i=0;i<5;i++)
@@ -92,6 +99,11 @@ function generateLevel()
     }
     else if(level==4)
     {
+        addObstacle(150,200,530,65,["One day some things matter, and another day some things doen't matter anymore.",
+                                    "You'll never know.",
+                                    "You can't predict what will matter someday."]);
+        addObstacle(150,400,320,30,["Do you know what REALLY matter in this world?"]);
+        addObstacle(550,480,120,30,["CONNECTIONS."]);
         for(i=0;i<31;i++)
             addRandomObject(3);
         for(i=0;i<9;i++)
@@ -130,12 +142,28 @@ function generateLevel()
         //You won.
     }
 }
-function addRandomObject(type)
+function addObstacle(x,y,w,h,text)
 {
     tmp=new Object();
+    tmp.x=x;
+    tmp.y=y;
+    tmp.width=w;
+    tmp.height=h;
+    tmp.color="#FFF";
+    tmp.text=text;
+    obstacleObjects.push(tmp);
+}
+function addRandomObject(type)
+{
+    unplaceable=true;
+    tmp=new Object();
     tmp.size=16;
-    tmp.x=rand(tmp.size*2,canvasW-tmp.size-100);
-    tmp.y=rand(tmp.size*2,canvasH-tmp.size-200);
+    do
+    {
+        tmp.x=rand(tmp.size*2,canvasW-tmp.size);
+        tmp.y=rand(tmp.size*2,canvasH-tmp.size);
+    }
+    while(insideObstacle(tmp));
     tmp.rotation=rand(0,360);
     tmp.dx=rand(-5,5);
     tmp.dy=rand(-5,5);
@@ -145,6 +173,49 @@ function addRandomObject(type)
     tmp.isFilled=false;
     tmp.ignoreCollision=false;
     drawableObjects.push(tmp); 
+}
+//check if there are obstacles in the middle of A and B
+function ammissiblePath(a,b)
+{
+    var res=true;
+    if(b==exitObject)
+        return true;
+    cx=(a.x+b.x)/2;
+    cy=(a.y+b.y)/2;
+    tmp=new Object();
+    tmp.size=a.size;
+    tmp.x=a.x;
+    tmp.y=a.y;
+    tmp.dx=(cx-a.x)/20;
+    tmp.dy=(cy-a.y)/20;
+
+    for(i=0;i<40;i++)
+    {
+        tmp.x+=tmp.dx;
+        tmp.y+=tmp.dy;
+        if(insideObstacle(tmp))
+        {
+            res=false;
+            break;
+        }
+    }
+    return res;
+}
+//return true if thing is inside one of the obstacleObjects
+function insideObstacle(thing)
+{
+    var res=false;
+    if(thing.x+thing.size>canvasW-180 && thing.y+thing.size>canvasH-80)
+        return true;
+    obstacleObjects.forEach(function(e)
+    {
+        if(thing.x+thing.size>e.x && thing.x-thing.size<e.x+e.width && thing.y+thing.size>e.y && thing.y-thing.size<e.y+e.height)
+        {
+            res=true;
+            return true;
+        }
+    });
+    return res;
 }
 function run()
 {
@@ -160,6 +231,7 @@ function run()
 
     if(level>0)
     {
+        ctx.fillStyle="#FFF";
         ctx.fillRect(canvasW-180,canvasH-80,250,2);
         ctx.fillRect(canvasW-180,canvasH-80,2,80);
         ctx.font = "20px Arial";
@@ -189,13 +261,39 @@ function run()
                     e.dy*=-1;
                 if(e.x+e.dx+e.size>canvasW-180 && e.y+e.size>canvasH-80)
                     e.dx*=-1;
+                //obstacles
+                obstacleObjects.forEach(function(o)
+                {
+                    //inside X
+                    if(e.x+e.size>o.x && e.x-e.size<o.x+o.width && e.y+e.size+e.dy>o.y && e.y-e.size+e.dy<o.y+o.height)
+                        e.dy*=-1;
+                    //inside Y
+                    if(e.y+e.size>o.y && e.y-e.size<o.y+o.height && e.x+e.size+e.dx>o.x && e.x-e.size+e.dx<o.x+o.width)
+                        e.dx*=-1;
+                });
             }
+        });
+        //draw obstacles
+        obstacleObjects.forEach(function(e)
+        {
+            ctx.fillStyle="#FFF";
+            ctx.fillRect(e.x,e.y,e.width,2);
+            ctx.fillRect(e.x,e.y,2,e.height);
+            ctx.fillRect(e.x+e.width-2,e.y,2,e.height);
+            ctx.fillRect(e.x,e.y+e.height-2,e.width,2);
+            ctx.font = "14px Arial";
+            for(i=0;i<e.text.length;i++)
+                ctx.fillText(e.text[i],e.x+10,20+e.y+i*15);
         });
         //something is selected
         if(selectedObject!=null && hoveredObject!=null)
         {
             ctx.beginPath();
-            ctx.strokeStyle="#FFF";
+            //check if we have obstacles in the middle
+            if(ammissiblePath(selectedObject,hoveredObject))
+                ctx.strokeStyle="#6F6";
+            else
+                ctx.strokeStyle="#600";  
             ctx.moveTo(hoveredObject.x,hoveredObject.y);
             ctx.lineTo(selectedObject.x,selectedObject.y);
             ctx.lineWidth = 3;
@@ -421,7 +519,8 @@ function mossoMouse(evt)
         hoveredObject=getObjectInsideMouse();
         if(hoveredObject!=null && hoveredObject.type!=selectedObject.type)
             hoveredObject=null;
-    }        
+    }
+    //console.log("mouse "+mousex+" "+mousey);        
 }
 function rilasciatoMouse(evt)
 {
@@ -430,7 +529,7 @@ function rilasciatoMouse(evt)
     if(selectedObject!=null)
     {
         //we need to merge
-        if(hoveredObject!=null)
+        if(hoveredObject!=null && ammissiblePath(selectedObject,hoveredObject))
         {
             hoveredObject.isFilled=true;
             mergeObjectA=selectedObject;
@@ -438,7 +537,9 @@ function rilasciatoMouse(evt)
             
             mergeObjectC=new Object();
             mergeObjectC.dx=mergeObjectA.dx+mergeObjectB.dx;
+            mergeObjectC.dx=mergeObjectC.dx%5;
             mergeObjectC.dy=mergeObjectA.dy+mergeObjectB.dy;
+            mergeObjectC.dy=mergeObjectC.dy%5;
             mergeObjectC.rotation=0;
             mergeObjectC.dr=mergeObjectA.dr+mergeObjectB.dr;
             mergeObjectC.ignoreCollision=false;
@@ -446,7 +547,7 @@ function rilasciatoMouse(evt)
             cx=(mergeObjectA.x+mergeObjectB.x)/2;
             cy=(mergeObjectA.y+mergeObjectB.y)/2;
             mergeObjectA.dx=(cx-mergeObjectA.x)/20;
-            mergeObjectA.dy=(cy-selectedObject.y)/20;
+            mergeObjectA.dy=(cy-mergeObjectA.y)/20;
             if(mergeObjectB==exitObject)
             {
                 mergeObjectA.ignoreCollision=true;
